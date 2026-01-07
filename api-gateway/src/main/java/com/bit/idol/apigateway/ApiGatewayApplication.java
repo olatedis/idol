57462e8d355912eb0bc3c2f1cc2cbed7150636d9
tw <tw4380833@naver.com>
2026-01-07 12:17:53 +0900

    의존성 변경

--- api-gateway/src/main/java/com/bit/idol/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bit/idol/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.bit.idol.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
