4fd79d263a725887957751dfbbea25fef4dcb435
BIT <1harrykim@naver.com>
2026-01-13 15:05:16 +0900

    notify-service 생성

---
+++ notify-service/src/main/java/com/bit/idol/notifyservice/NotifyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bit.idol.notifyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyServiceApplication.class, args);
+    }
+
+}
