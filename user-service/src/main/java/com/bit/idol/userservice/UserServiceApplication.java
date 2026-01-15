d109b82c7f6e6b6775497bd388b17056cbac85d5
tw <tw4380833@naver.com>
2026-01-15 11:30:02 +0900

    유저데이터 캐시 저장

--- user-service/src/main/java/com/bit/idol/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/bit/idol/userservice/UserServiceApplication.java
@@ -2,10 +2,12 @@ package com.bit.idol.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCaching // 캐싱 기능 활성화
 public class UserServiceApplication {
 
     public static void main(String[] args) {
