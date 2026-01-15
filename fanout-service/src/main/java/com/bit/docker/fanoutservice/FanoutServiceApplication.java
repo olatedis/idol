c061e07f71bb0111c3cbe887ca33f91ac250d79a
BIT <1harrykim@naver.com>
2026-01-15 12:46:17 +0900

    일단 fanout-service모듈 생성

---
+++ fanout-service/src/main/java/com/bit/docker/fanoutservice/FanoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bit.docker.fanoutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanoutServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanoutServiceApplication.class, args);
+    }
+
+}
