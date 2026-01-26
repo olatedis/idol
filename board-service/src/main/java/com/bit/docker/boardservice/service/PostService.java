a55f5d32c14fdb83f537f2e4f57ff70d974dff32
BIT <1harrykim@naver.com>
2026-01-26 16:39:24 +0900

    board-service build.gradle수정

--- board-service/src/main/java/com/bit/docker/boardservice/service/PostService.java
+++ board-service/src/main/java/com/bit/docker/boardservice/service/PostService.java
@@ -20,6 +20,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
