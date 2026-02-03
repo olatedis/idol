4e40edd2bed1b97728be14a9d3384ae5e1ee0f22
tw <tw4380833@naver.com>
2026-02-03 09:33:29 +0900

    XSS 방어

--- chat-service/src/main/java/com/bit/idol/chatservice/config/WebSocketConfig.java
+++ chat-service/src/main/java/com/bit/idol/chatservice/config/WebSocketConfig.java
@@ -1,13 +1,20 @@
 package com.bit.idol.chatservice.config;
 
 import com.bit.idol.chatservice.handler.StompHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -15,11 +22,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final StompHandler stompHandler;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 클라이언트 연결 엔드포인트: ws://localhost:8089/ws-chat
-        // SockJS 제거 (Postman/Apic 테스트 용이성 및 성능 향상)
         registry.addEndpoint("/ws-chat")
                 .setAllowedOriginPatterns("*");
     }
@@ -37,5 +44,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureClientInboundChannel(ChannelRegistration registration) {
         // 연결 시 토큰 검증 및 구독 권한 체크 인터셉터 등록
         registration.interceptors(stompHandler);
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(objectMapper); // XSS 설정이 적용된 ObjectMapper 사용
+        converter.setContentTypeResolver(resolver);
+        
+        messageConverters.add(converter);
+        return false; // 기본 컨버터 추가 방지
     }
 }
