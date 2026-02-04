8c0938eb71e7068caf74c612765b6f519f45362d
olatedis <kms0000304@gmail.com>
2026-02-04 17:05:50 +0900

    TestPage 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,14 @@
 import './App.css'
-import PaymentTestPage from "./pages/paymentTest/PaymentTestPage.tsx";
+
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-import SuccessTestPage from "./pages/paymentTest/SuccessTestPage.tsx";
-import SubscriptionPage from "./pages/subscription/SubscriptionPage.tsx";
-import SubscriptionSuccessPage from "./pages/subscription/SubscriptionSuccessPage.tsx";
-import SubscriptionFailPage from "./pages/subscription/SubscriptionFailPage.tsx";
+import MainPage from "./pages/test/MainPage.tsx";
 
 function App() {
   return (
     <>
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<PaymentTestPage />} />
-                <Route path="/success" element={<SuccessTestPage />} />
-                <Route path="/subscription" element={<SubscriptionPage />} />
-                <Route path="/subscription/success" element={<SubscriptionSuccessPage />} />
-                <Route path="/subscription/fail" element={<SubscriptionFailPage />} />
+                <Route path="/" element={<MainPage />} />
             </Routes>
 
         </BrowserRouter>
