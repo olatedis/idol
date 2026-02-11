a9b708b047bc9e4f3f92830bb2688333450219fd
tw <tw4380833@naver.com>
2026-02-11 15:12:37 +0900

    UTC 기준 시간 변경

--- api-gateway/src/main/java/com/bit/idol/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/bit/idol/apigateway/ApiGatewayApplication.java
@@ -1,10 +1,13 @@
 package com.bit.idol.apigateway;
 
 import com.bit.idol.apigateway.config.JwtProperties;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,4 +18,8 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
--- vote-service/src/main/java/com/bit/idol/voteservice/VoteServiceApplication.java
+++ vote-service/src/main/java/com/bit/idol/voteservice/VoteServiceApplication.java
@@ -1,10 +1,13 @@
 package com.bit.idol.voteservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,4 +19,8 @@ public class VoteServiceApplication {
         SpringApplication.run(VoteServiceApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
